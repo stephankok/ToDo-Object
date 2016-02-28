48a089a0327c4d5d428ea742795e6ee69d98632e
stephankok <stephankok33@gmail.com>
2016-02-28 12:39:15 +0100

    Deleted pictures  added make tekst green when pressed
     commented

--- app/src/main/java/com/example/stephan/todo/MainActivity.java
+++ app/src/main/java/com/example/stephan/todo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.stephan.todo;
 
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,16 +15,19 @@ import java.util.Scanner;
  * A To Do app created by Stephan Kok.
  * It will create items with an own created adapter MyOwnRowAdapter.
  * You can add items by pressing the button and you can delete items by longclicking on it.
- * It will save all your items on a file so you wont lose them!
+ * Feature: You can make items green by pressing them
+ * It will save all your items on a file so you wont lose them! Data is saved per line,
+ * so make sure users cannot enter a new line.
  */
 public class MainActivity extends AppCompatActivity {
 
     // Initialize values.
-    EditText addItemToList;
-    ArrayList<String> itemsOnList = new ArrayList<String>();
-    ListView listView;
-    MyOwnRowAdapter myadepter;
-    Boolean saveDataOnFile = true;
+    EditText addItemToList;                                    // Get To Do from user
+    ArrayList<String> itemsOnList = new ArrayList<String>();   // Save all items of To Do List
+    ListView listView;                                         // Place adapter here
+    MyOwnRowAdapter myadepter;                                 // Make adapter
+    Boolean saveDataOnFile = true;                             // Ensure data is saved
+    ArrayList<Integer> colorData = new ArrayList<Integer>();   // Save the color of To Do list items
 
     /**
      * Called on create.
@@ -35,15 +39,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // find
+        // find elements
         addItemToList = (EditText) findViewById(R.id.addItemEditText);
         listView = (ListView) findViewById(R.id.listView);
 
-        // read a file and add it to itemsOnList.
-        readDataFromFile(itemsOnList);
+        // read a file and add it to itemsOnList an colorData
+        readDataFromFile(itemsOnList, colorData);
 
         // make adapter
-        myadepter = new MyOwnRowAdapter(this, itemsOnList, saveDataOnFile);
+        myadepter = new MyOwnRowAdapter(this, itemsOnList, saveDataOnFile, colorData);
 
         // add adapter to listview
         listView.setAdapter(myadepter);
@@ -62,7 +66,10 @@ public class MainActivity extends AppCompatActivity {
             addItemToList.setText("");
 
             // add item
-            myadepter.insert(itemToAdd,0);
+            myadepter.insert(itemToAdd, 0);
+
+            // add color
+            myadepter.insertColor();
 
             // update adapter and listView.
             myadepter.notifyDataSetChanged();
@@ -70,19 +77,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * !WARNING Reads all data from STORETEXT
-     * Get all data in file and add them to placeReadedData
+     * !WARNING Reads all data from STORETEXT. Read apdapter to see how saving works.
+     * Get all data in file and add them to placeReadedData.
      */
-    public void readDataFromFile(ArrayList<String> placeReadedData){
+    public void readDataFromFile(ArrayList<String> placeReadedData, ArrayList<Integer> placeColorData ){
         try {
-            // read a file
+            // Open File
             Scanner scan = new Scanner(openFileInput("STORETEXT"));
+
+            // Find all To Do items
             while (scan.hasNextLine()) {
                 String line = scan.nextLine();
+
+                // If this line is found you will get color id's
+                if(line.compareTo("END OF THE ITEMS NOW COLORS. code:182838") == 0){
+                    break;
+                }
+
+                // if line is not empty it is data, add it.
                 if (!line.isEmpty()){
                     placeReadedData.add(line);
                 }
             }
+
+            // Now find all Color's
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine();
+
+                if (!line.isEmpty()){
+                    placeColorData.add(Integer.parseInt(line));
+                }
+            }
+
+            // done
             scan.close();
         } catch (IOException e) {
             e.printStackTrace();
--- app/src/main/java/com/example/stephan/todo/MyOwnRowAdapter.java
+++ app/src/main/java/com/example/stephan/todo/MyOwnRowAdapter.java
@@ -17,39 +17,52 @@ import java.util.ArrayList;
 /**
  * An adapter Created By Stephan Kok.
  *
- *  !WARNING! When u set saveOnFile to true it will save all items on STORETEXT
- *  set false to disable
+ *  !WARNING! When u set saveOnFile to true it will save all items on STORETEXT. First all
+ *  data is saved then the line: END OF THE ITEMS NOW COLORS. code:182838 is written en then all
+ *  the colors are saved.
+ *  Set saveOnFile false to disable saving.
  *
  *  Input:
  *  an ArrayList<String> of items to add. You can dynamically add or remove.
+ *  an ArrayList<Integer> of color id's. When you add an item it is automatic black.
+ *      You must make all color Color.BLACK or Color.Green.
  *
  *  Your layout wil need:
  *  TextView with id: nameTextView
  *
+ *  Your strings xml will need:
+ *  EMColorsAdapter: error message when collors cant load.
+ *  standardError: standard error it will add the problem to string.
+ *
  *  Features:
  *  It will delete an item that is long pressed.
+ *  Items will turn green when pressing them
  */
 public class MyOwnRowAdapter extends ArrayAdapter<String> {
 
     Context context;                // Activity to display the adapter
     ArrayList<String> itemOnList;   // Items of the to do list
     Boolean fileSaving;             // Set true to save items on STORETEXT
+    ArrayList<Integer> itemColor;   // color of the items
 
     /**
      * Initialize MMyOwnRowAdapter
      */
-    public MyOwnRowAdapter(Context contextOfApp, ArrayList<String> itemsOfToDoList, Boolean saveOnFile){
+    public MyOwnRowAdapter(Context contextOfApp, ArrayList<String> itemsOfToDoList, Boolean saveOnFile, ArrayList<Integer> colorData){
         super(contextOfApp, R.layout.single_row_layout, itemsOfToDoList);
 
         context = contextOfApp;
         itemOnList = itemsOfToDoList;
         fileSaving = saveOnFile;
+        itemColor = colorData;
     }
 
     /**
      * Add the picture and itemtoadd to the Listview.
      *
      * Set a onlongclick listener that will delete an item when it is long clicked
+     *
+     * set onclick listener that will turn items green.
      */
     public View getView(final int position, View view, ViewGroup parent){
         if(view == null){
@@ -65,21 +78,35 @@ public class MyOwnRowAdapter extends ArrayAdapter<String> {
 
         // set text on textview
         textview.setText(name);
+        try{
+            textview.setTextColor(itemColor.get(position));
+        }catch (Throwable t) {
+            // error happened.
+            t.printStackTrace();
+            Toast.makeText(context, R.string.EMColorsAdapter, Toast.LENGTH_SHORT).show();
+        }
 
-        // The longClickListener
+        // The longClickListener, delete items
         View.OnLongClickListener longclicklistener = new View.OnLongClickListener(){
 
             @Override
             public boolean onLongClick(View view) {
-                // make anousment
+                // make announcement
                 String text = context.getString(R.string.youDeleted) + name;
 
                 // make popup
-                Toast toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
-                toast.show();
+                Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
 
                 // delete item
                 itemOnList.remove(position);
+
+                try{
+                    itemColor.remove(position);
+                }catch (Throwable t) {
+                    // error happened.
+                    t.printStackTrace();
+                    Toast.makeText(context, R.string.EMColorsAdapter, Toast.LENGTH_LONG).show();
+                }
 
                 // update listview
                 notifyDataSetChanged();
@@ -88,22 +115,42 @@ public class MyOwnRowAdapter extends ArrayAdapter<String> {
             }
         };
 
+        // the onclick listener, turn items green
         View.OnClickListener shortlistener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Check if green, make black
                 if(textview.getTextColors() == ColorStateList.valueOf(Color.GREEN)){
                     textview.setTextColor(Color.BLACK);
+                    itemColor.set(position,Color.BLACK);
                 }
+                // else it is black, make it green
                 else {
                     textview.setTextColor(Color.GREEN);
+                    itemColor.set(position, Color.GREEN);
+                }
+
+                // save data, you wont need to update listview and adapter
+                if(fileSaving){
+                    updateAllData();
                 }
             }
         };
 
+        // add all listeners to View
         view.setOnLongClickListener(longclicklistener);
         view.setOnClickListener(shortlistener);
 
         return view;
+    }
+
+
+    /**
+     *  Add color in front.
+     *  It must be black so requires no input
+     */
+    public void insertColor(){
+        itemColor.add(0,Color.BLACK);
     }
 
     /**
@@ -122,6 +169,11 @@ public class MyOwnRowAdapter extends ArrayAdapter<String> {
      * When fileSaving is set true, this function will be called.
      * It will open/create a file STORETEXT.
      * It will save all itemsOnList and separate them by a newline.
+     *
+     * To seperate data from colors there will be writen a special line:
+     * END OF THE ITEMS NOW COLORS. code:182838
+     *
+     * Then all colors id will be added in string format and will be seperated by a newline.
      */
     public void updateAllData(){
         try {
@@ -133,10 +185,21 @@ public class MyOwnRowAdapter extends ArrayAdapter<String> {
                 out.println(itemOnList.get(i) + "\n");
             }
 
+            // add special line.
+            out.println("END OF THE ITEMS NOW COLORS. code:182838\n");
+
+            // add all colors
+            for (int i = 0; i < itemColor.size(); i++){
+                out.println(itemColor.get(i).toString() + "\n");
+            }
+
+            // close file
+            out.close();
+
         } catch (Throwable t) {
             // error happened.
             t.printStackTrace();
-            Toast.makeText(context, "An Error Occurred: " + t.toString(), Toast.LENGTH_LONG).show();
+            Toast.makeText(context, R.string.standardError + t.toString(), Toast.LENGTH_LONG).show();
 
         }
     }
